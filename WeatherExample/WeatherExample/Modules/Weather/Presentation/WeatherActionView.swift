9590c6a92a053f135c187c637cf4f169178c7ff5
qazi <qazi.naveed@nextgeni.com>
2025-01-08 23:24:39 +0100

    adding sample UI

--- WeatherExample/WeatherExample/Modules/Weather/Presentation/WeatherActionView.swift
+++ WeatherExample/WeatherExample/Modules/Weather/Presentation/WeatherActionView.swift
@@ -8,8 +8,83 @@
 import SwiftUI
 
 struct WeatherActionView: View {
+    @State private var cityName: String = ""
+    @FocusState private var isFocused: Bool
+    
+    // Constants
+    private let verticalSpacing: CGFloat = 24
+    private let subViewHeight: CGFloat = 56
+    private let textFieldPadding: CGFloat = 16
+    private let cornerRadius: CGFloat = 16
+    private let buttonPadding: CGFloat = 12
+    private let horizontalPadding: CGFloat = 24
+    private let topPadding: CGFloat = 32
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(alignment: .leading, spacing: verticalSpacing) {
+                // Title Text
+                titleView()
+                
+                // City Input TextField
+                cityInputField()
+                
+                // Weather Forecast Button
+                forecastButton()
+                
+                Spacer()
+            }
+            .padding(.horizontal, horizontalPadding)
+            .padding(.top, topPadding)
+            .background(AppColors.primaryBackground.color)
+            .toolbarBackground(AppColors.navBarColor.color, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .navigationBarTitle(AppConstants.DisplayMessages.header, displayMode: .inline)
+        }
+    }
+    
+    // MARK: - Subviews
+    
+    /// Title View
+    private func titleView() -> some View {
+        ExampleViewWithTheme(AppConstants.DisplayMessages.forecastText, style: .heading2)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    /// City Input Field
+    private func cityInputField() -> some View {
+        TextField(AppConstants.DisplayMessages.enterCityText, text: $cityName)
+            .padding(.horizontal, textFieldPadding)
+            .frame(height: subViewHeight)
+            .background(
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(isFocused ? AppColors.accent.color : AppColors.textBorder.color, lineWidth: 1)
+            )
+            .focused($isFocused)
+            .font(ThemeFonts.textRegular())
+            .opacity(isFocused ? 1 : 0.8)
+            .foregroundColor(AppColors.textPrimary.color)
+            .autocorrectionDisabled(true)
+            .textInputAutocapitalization(.never)
+            .keyboardType(.default)
+    }
+    
+    /// Weather Forecast Button
+    private func forecastButton() -> some View {
+        Button(action: fetchWeatherForecast) {
+            Text(AppConstants.DisplayMessages.forecastButtonTitle)
+                .font(ThemeFonts.title())
+                .foregroundColor(.white)
+                .padding(.vertical, buttonPadding)
+                .frame(maxWidth: .infinity)
+                .frame(height: subViewHeight)
+                .background(Capsule().fill(AppColors.accent.color))
+        }
+    }
+    
+    // MARK: - Actions
+    private func fetchWeatherForecast() {
+        print("Fetching weather forecast for \(cityName)")
     }
 }
 
