92bdf077a919f1e2e47607ba198c9b397763cc29
qazi <qazi.naveed@nextgeni.com>
2025-01-08 21:18:10 +0100

    adding WeatherActionView and removed default contentView

---
+++ WeatherExample/WeatherExample/Modules/Weather/Presentation/WeatherActionView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherActionView.swift
+//  WeatherExample
+//
+//  Created by Qazi on 08/01/2025.
+//
+
+import SwiftUI
+
+struct WeatherActionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherActionView()
+}
--- WeatherExample/WeatherExample/WeatherExampleApp.swift
+++ WeatherExample/WeatherExample/WeatherExampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WeatherExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WeatherActionView()
         }
     }
 }
