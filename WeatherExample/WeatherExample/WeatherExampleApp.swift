89c932088c09223b71bbfc4d442c5bd0211c3f74
qazi <qazi.naveed@nextgeni.com>
2025-01-08 21:04:59 +0100

    Created a WeatherExample Project

---
+++ WeatherExample/WeatherExample/WeatherExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherExampleApp.swift
+//  WeatherExample
+//
+//  Created by Qazi on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherExample/WeatherExampleTests/WeatherExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherExampleTests.swift
+//  WeatherExampleTests
+//
+//  Created by Qazi on 08/01/2025.
+//
+
+import Testing
+@testable import WeatherExample
+
+struct WeatherExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
