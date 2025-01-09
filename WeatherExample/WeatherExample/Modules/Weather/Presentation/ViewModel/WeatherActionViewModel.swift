a578632cf54540e40a5ce14d31955658d101d48f
qazi <qazi.naveed@nextgeni.com>
2025-01-09 01:35:28 +0100

    show forecastview  via container

--- WeatherExample/WeatherExample/Modules/Weather/Presentation/ViewModel/WeatherActionViewModel.swift
+++ WeatherExample/WeatherExample/Modules/Weather/Presentation/ViewModel/WeatherActionViewModel.swift
@@ -9,22 +9,32 @@ import SwiftUI
 protocol WeatherActionViewModelProtocol: ObservableObject {
     var interactor: WeatherInteractorProtocol { get }
     var isLoading: Bool { get set }
+    var isWeatherViewPresented: Bool { get set }
+    var cityName: String { get set }
     func forecastButtonTapped()
 }
 
 class WeatherActionViewModel: WeatherActionViewModelProtocol {
     var interactor: WeatherInteractorProtocol
-    var isLoading: Bool = false
+    @Published var isLoading: Bool = false
+    @Published var isWeatherViewPresented: Bool = false
+    @Published var cityName: String = ""
+
+    var weatherView: AnyView?
     
     init(interactor: WeatherInteractorProtocol = WeatherInteractor()) {
         self.interactor = interactor
     }
     
+    @MainActor
     func forecastButtonTapped() {
         Task {
+            
             do {
-                
-                let view = try await interactor.fetchWeatherView(cityName: "cityName")
+                let view = try await interactor.fetchWeatherView(cityName: cityName)
+                self.weatherView = view
+                self.isLoading = false
+
             } catch {
                 print(error)
             }
